faa1b25696d3516367252399f8a7bbea0a6653b9
KIMHEEBUM <kbum0107@naver.com>
2020-10-13 00:50:38 +0900

    Add Text File[bread.php]

---
+++ bread.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang = "ko">
+<head>
+   <meta charset="UTF-8">
+   <title>Bread Reservation Service</title>
+   <style>
+      #aa{display: inline;}
+      #aa{display: inline-block;}
+      p{
+         font-family: 'Nanum Barun Gothic', sans-serif;
+            font-weight : 300;
+         font-size: large;
+         color: black; 
+         position: relative;
+         left: 1325px;
+         top: -50px;
+      }
+      h1{
+            font-family: 'Nanum Barun Gothic', sans-serif;
+            font-weight : 300;
+         font-size: large;
+         color: white;
+         width: 208px; 
+         background-color: rgba(0,0,0,0.6);
+         position: relative;
+         left: 20px;
+         top: 10px;
+      }
+      #circle1{
+         background-color:rgba(222,184,135,0.7);
+         width:198px;
+         height:198px;
+         border-radius:96px;
+         text-align:center;
+         color:white;
+         font-size:25px;
+         line-height:135px;
+         position : relative;
+         left: 50px;
+         top: 300px;
+         box-shadow:0px 13px 10px #cbc9c6;
+      }
+      #circle2{
+         background-color:rgba(225,164,86,0.7);
+         width:198px;
+         height:198px;
+         border-radius:96px;
+         text-align:center;
+         color:white;
+         font-size:25px;
+         line-height:135px;
+         position : relative;
+         left: 350px;
+         top: 80px;
+         box-shadow:0px 13px 10px #cbc9c6;
+      }
+      #circle3{
+         background-color:rgba(255,228,181,0.7);
+         width:198px;
+         height:198px;
+         border-radius:96px;
+         text-align:center;
+         color:white;
+         font-size:25px;
+         line-height:135px;
+         position : relative;
+         left: 650px;
+         top: -140px;
+         box-shadow:0px 13px 10px #cbc9c6;
+      }
+      #circle4{
+         background-color:rgba(249,196,24,0.7);
+         width:198px;
+         height:198px;
+         border-radius:96px;
+         text-align:center;
+         color:white;
+         font-size:25px;
+         line-height:135px;
+         position : relative;
+         left: 950px;
+         top: -360px;
+         box-shadow:0px 13px 10px #cbc9c6;
+      }
+      #circle5{
+         background-color:rgba(199,196,24,0.7);
+         width:198px;
+         height:198px;
+         border-radius:96px;
+         text-align:center;
+         color:white;
+         font-size:25px;
+         line-height:135px;
+         position : relative;
+         left: 1250px;
+         top: -580px;
+         box-shadow:0px 13px 10px #cbc9c6;
+      }
+      .point{
+         position:absolute;
+         text-align:center;
+         top:50px;
+         left:85px;
+      }
+   </style>
+</head>
+<body>
+   
+      <h1>Bread Reservation Service</h1>
+      <img src= "Banner.jpg" width="1460" height="300" style="
+         position:absolute;
+         top:70px;
+
+      }">
+      
+      <p id="aa">Log IN</p>
+      <p id="aa">|</p>
+      <P id="aa">HOME</P>
+   <div id="circle1">
+      메뉴<br>
+      <div class="point"> -></div></br>
+
+   </div>
+   <br>
+   <div id="circle2">
+      장바구니<br>
+      <div class="point"> -></div></br>
+
+   </div>
+   <br>
+   <div id="circle3">
+      주문내역<br>
+      <div class="point"> -></div></br>
+
+   </div>
+   <br>
+   <div id="circle4">
+      마일리지<br>
+      <div class="point"> -></div></br>
+
+   </div>
+   <br>
+   <div id="circle5">
+      쿠킹 레시피<br>
+      <div class="point"> -></div></br>
+
+   </div>
+
+</body>
+</html>
